1c3c63088d1115e514c72bd68f00d6cd265e8ea5
Anton <poverenosn@gmail.com>
2025-04-02 13:28:45 +0300

    step4

--- database.sql
+++ database.sql
@@ -3,3 +3,13 @@ CREATE TABLE urls (
     name VARCHAR(255) UNIQUE NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+CREATE TABLE url_checks (
+    id SERIAL PRIMARY KEY,
+    url_id INTEGER NOT NULL REFERENCES url(id) ON DELETE CASCADE,
+    status_code INTEGER,
+    h1 VARCHAR(255),
+    title VARCHAR(255),
+    description TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -16,11 +16,27 @@ app.secret_key = os.urandom(24)
 # Инициализируем SQLAlchemy
 db = SQLAlchemy(app)
 
+
 # Модель для таблицы URLs
 class URL(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255), nullable=False, unique=True)
     created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
+
+
+class URLCheck(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    url_id = db.Column(db.Integer, db.ForeignKey('url.id'), nullable=False)
+    status_code = db.Column(db.Integer, nullable=True)
+    h1 = db.Column(db.String(255), nullable=True)
+    title = db.Column(db.String(255), nullable=True)
+    description = db.Column(db.Text, nullable=True)
+    created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
+
+
+# Связь между таблицами
+URL.checks = db.relationship('URLCheck', backref='url', lazy=True)
+
 
 # Главная страница с формой ввода
 @app.route('/', methods=['GET', 'POST'])
@@ -58,18 +74,33 @@ def index():
     return render_template('index.html')  # Рендерим главную
 
 
-
 # Страница с информацией об отдельном URL
 @app.route('/urls/<int:id>')
 def view_url(id):
     url = URL.query.get_or_404(id)
     return render_template('view_url.html', url=url)
 
+
 # Страница с выводом всех URL
 @app.route('/urls')
 def list_urls():
     urls = URL.query.order_by(URL.created_at.desc()).all()
     return render_template('list_urls.html', urls=urls)
+
+
+# Создание новой проверки
+@app.route('/urls/<int:id>/checks', methods=['POST'])
+def create_check(id):
+    url = URL.query.get_or_404(id)
+
+    # Создаем новую запись в таблице проверок
+    new_check = URLCheck(url_id=url.id)
+    db.session.add(new_check)
+    db.session.commit()
+
+    flash("Проверка успешно добавлена.")
+    return redirect(url_for('view_url', id=id))
+
 
 if __name__ == "__main__":
     with app.app_context():  # Создание контекста приложения
