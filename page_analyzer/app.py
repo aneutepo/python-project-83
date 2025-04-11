9cd3fb60e7521e0a6c424d2014953fc88dbafe55
Anton <poverenosn@gmail.com>
2025-04-11 13:26:54 +0300

    step1

---
+++ page_analyzer/app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+from dotenv import load_dotenv
+import os
+
+
+app = Flask(__name__)
+
+load_dotenv()
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+
+@app.route('/')
+def index():
+    return "Я живая главная страница"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
